7e5e56385b737901646e5d2af80eab5202c98d1c
Melody Mo <mmo@Calmia.local>
2026-06-11 09:15:07 -0400

    updated scripts for embedding

--- scripts/get_emb_rinalmo.sh
+++ scripts/get_emb_rinalmo.sh
@@ -1,16 +1,8 @@
-# Get the embedding of the RNA-FM model
+# Get the embedding of the RiNALMo model
 
 poetry run python ../preprocess/get_seq_embedding.py \
-		--i ../data/human/gencode44_utr_gene_unique_cdhit09.csv \
-		--o ../data/human/gencode44_embedding_rinalmo_whole_ave_cdhit09.pt \
-		--rinalmo \
-		--rinalmo_method whole_ave \
-		--format pt \
-
-
-poetry run python ../preprocess/get_seq_embedding.py \
-		--i ../data/mouse/gencode_vM33_utr_gene_unique_cdhit09.csv \
-		--o ../data/mouse/gencode_vM33_embedding_rinalmo_whole_ave_cdhit09.csv \
+		--i ../data/raw_data.csv \
+		--o ../data/raw_data_embedding_rinalmo_whole_ave.pt \
 		--rinalmo \
 		--rinalmo_method whole_ave \
 		--format pt \
--- scripts/get_emb_rnafm.sh
+++ scripts/get_emb_rnafm.sh
@@ -1,12 +1,6 @@
 # Get the embedding of the RNA-FM model
 
 poetry run python ../preprocess/get_seq_embedding.py \
-		--i ../data/human/gencode44_utr_gene_unique_cdhit09.csv \
-		--o ../data/human/_test_gencode44_embedding_forward_cdhit09.pkl \
-		--over_length average \
-
-
-poetry run python ../preprocess/get_seq_embedding.py \
-		--i ../data/mouse/gencode_vM33_utr_gene_unique_cdhit09.csv \
-		--o ../data/mouse/_test_gencode_vM33_embedding_ave_cdhit09.pkl \
+		--i ../data/raw_data.csv \
+		--o ../data/raw_data_embedding_rnafm_ave.pkl \
 		--over_length average \
